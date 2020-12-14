9113c2894c81ca5b9ca8b2c17dd42bcb66c10abc
Proxy7R4C3R <proxyr025@gmail.com>
2020-12-14 13:38:49 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const http = require('http')
+const fs = require('fs')
+const port = 80
+
+const server = http.createServer(function(req,res){
+    res.writeHead(200,{'Content-Type': 'text/html'})
+    fs.readFile('index.html',function(error,data){
+
+        if (error) {
+            res.writeHead(404)
+            res.write('Content Not Found')
+        }else {
+            res.write(data)
+        }
+        res.end()
+    })
+    
+})
+
+
+server.listen(port, function(error){
+
+    if(error){
+
+        console.log('Something Failed',error)
+    }else {
+        console.log('Server is Listning on' + port)
+    }
+})
